c309276789b0f45d4dc3b6cfa41dc45f97f4c92e
Carlos Gomez <gomezcarlose@gmail.com>
2020-01-07 11:23:34 -0300

    add AOP logger

---
+++ src/main/java/com/example/demosp/Loggable.java
@@ -0,0 +1,20 @@
+package com.example.demosp;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class Loggable {
+
+	Logger LOGGER = LoggerFactory.getLogger(Loggable.class);
+	
+	@AfterReturning(pointcut="execution(* com.example.demosp.*.*(..))",returning="value")
+	public void logReturn(JoinPoint jp, Object value) {
+		LOGGER.info("Class {} executed action: {} with returning value of: {}",jp.getSignature().getDeclaringTypeName() , jp.getSignature().getName(),value);
+	}
+}
